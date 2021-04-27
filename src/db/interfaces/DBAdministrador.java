12da4494feb6fe1e87c2d2e0064b385d611a3cfa
Gabriela-Levenfeld <43616917+Gabriela-Levenfeld@users.noreply.github.com>
2021-04-27 11:17:35 +0200

    Reorganizacion repositorio

--- src/db/interfaces/DBAdministrador.java
+++
@@ -1,9 +0,0 @@
-package db.interfaces;
-
-public interface DBAdministrador {
-	//Acceder a la BD e inicializarlo cuando corresponda
-	public void connect();
-
-	//Cerrar la conexión con la BD
-	public void disconnect();
-}
